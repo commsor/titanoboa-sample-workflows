3602561fa8a0c797d0c205d23f589ec46563e1b7
Miro Kubicek <miroslav.kubicek@gmail.com>
2020-12-05 08:09:43 +0100

    Create ext-dependencies.clj

---
+++ reclojure2020/ext-dependencies.clj
@@ -0,0 +1,13 @@
+{:coordinates [[io.titanoboa.tasklet/aws-ec2 "0.1.0"]
+               [io.titanoboa.tasklet/aws-s3 "0.1.0"]
+               [io.titanoboa.tasklet/pdf "0.1.0"]
+               [io.titanoboa.tasklet/http-client "0.1.1"]
+               [io.titanoboa.tasklet/kmer "0.1.0"]],
+ :require [[io.titanoboa.tasklet.aws.ec2]
+           [io.titanoboa.tasklet.aws.s3]
+           [io.titanoboa.tasklet.pdf]
+           [io.titanoboa.tasklet.httpclient]
+           [io.titanoboa.tasklet.kmer]],
+ :import [[nil]],
+ :repositories {"central" "https://repo1.maven.org/maven2/",
+                "clojars" "https://clojars.org/repo"}}
